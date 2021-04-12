acd1a48e9b2a87c97c732c242d03b7137f478726
saputrams <muhsukmas@gmail.com>
2021-04-13 00:41:03 +0700

    first

--- Klinik/Patient.aspx.designer.cs
+++ Klinik/Patient.aspx.designer.cs
@@ -11,16 +11,7 @@ namespace Klinik
 {
 
 
-    public partial class SiteMaster
+    public partial class Patient
     {
-
-        /// <summary>
-        /// MainContent control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
